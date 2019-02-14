551fb552f7145bad94327c70aec1a0a8d0a4d8d9
quietsj <quietsj@github.com>
2019-02-14 11:33:44 +0800

    update

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"github.com/astaxie/beego"
 )
 
-
-// env GOPATH=$GOPATH
+// sudo env GOPATH=$GOPATH bee run, scp - admin@39.108.12.40:/home/admin/go/src/air-quality-predict/
+// CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
 func main() {
+	//models.GetAirQualityIndexData()
 	beego.Run()
 }
